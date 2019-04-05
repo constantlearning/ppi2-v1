5f54ddcd250d2b9f265a4c65411b6fbf18d46a95
Lucas Gontijo <Lucasgontijo0@gmail.com>
2019-04-05 13:38:22 -0300

    First commit! 🎉

---
+++ src/test/java/com/unitri/ppi2v1/Ppi2V1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unitri.ppi2v1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ppi2V1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
