0d91076f81e83f2aaaec279766bf3677d6049564
Lucas Gontijo <Lucasgontijo0@gmail.com>
2019-04-05 13:39:06 -0300

    First commit! 🎉

---
+++ src/main/java/com/unitri/ppi2v1/Ppi2V1Application.java
@@ -0,0 +1,13 @@
+package com.unitri.ppi2v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ppi2V1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ppi2V1Application.class, args);
+    }
+
+}
